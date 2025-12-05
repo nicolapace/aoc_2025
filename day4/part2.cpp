83c3cf031cf44e720c151a282826b04f865aa56b
Nicola Pace <nicola.pace@cern.ch>
2025-12-05 15:22:02 +0100

    add day 4

---
+++ day4/part2.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int calculate_neighbors(const vector<vector<uint8_t>>& matrix, int x, int y) {
+    int directions[8][2] = {
+        {-1, -1}, {-1, 0}, {-1, 1},
+        {0, -1},          {0, 1},
+        {1, -1},  {1, 0},  {1, 1}
+    };
+    int count = 0;
+    for (auto& dir : directions) {
+        int newX = x + dir[0];
+        int newY = y + dir[1];
+        if (newX >= 0 && newX < matrix.size() && newY >= 0 && newY < matrix[0].size()) {
+            count += matrix[newX][newY];
+        }
+    }
+    return count;
+}
+
+long cleanup_neighbors_return_num_removed(vector<vector<uint8_t>>& matrix, vector<vector<string>>& char_matrix) {
+    long removed = 0;
+    auto matrix_copy = matrix;
+    for( int i=0; i<matrix.size(); i++) {
+        for (int j=0; j<matrix[0].size(); j++) {
+            if (matrix[i][j] == 1) {
+                int neighbors = calculate_neighbors(matrix, i, j);
+                if ( neighbors < 4 ){
+                    removed += 1;
+                    matrix_copy[i][j] = 0;
+                    char_matrix[i][j] = ".";
+                    std::cout << 'X';
+                } else {
+                    std::cout << char_matrix[i][j];
+                }
+            } else {
+                std::cout << char_matrix[i][j];
+            }
+        }
+        std::cout << std::endl;
+    }
+    matrix = matrix_copy;
+    return removed;
+}
+
+int main() {
+	std::string line;
+
+	std::ifstream MyReadFile("input.txt");
+	long total {};
+    vector<vector<uint8_t>> matrix;
+    vector<vector<string>> char_matrix;
+	while (std::getline (MyReadFile, line)) {
+        vector<uint8_t> row;
+        vector<string> char_row;
+        for ( auto c : line ) {
+            if(c == '@'){
+                row.push_back(1);
+                char_row.push_back("@");
+            } else if (c == '.'){
+                row.push_back(0);
+                char_row.push_back(".");
+            } else {
+                throw std::runtime_error("Unexpected character in input");
+            }
+        }
+        matrix.push_back(row);
+        char_matrix.push_back(char_row);
+	}
+	// Close the file
+	MyReadFile.close();
+
+    while (true) {
+        long removed = cleanup_neighbors_return_num_removed(matrix, char_matrix);
+        if ( removed == 0 ) {
+            break;
+        }
+        total += removed;
+        std::cout << "---- Removed this round: " << removed << " Total removed: " << total << " ----" << std::endl;
+    }
+    
+
+	std::cout << "Total: " << total << std::endl;
+
+}
